9dfb964531634077652915d13ff7040ff7193c68
Abd Eltawab Shaaban <105607290+scob337@users.noreply.github.com>
2024-07-22 21:13:07 +0300

    da

--- src/component/Testing/submit.js
+++ src/component/Testing/submit.js
@@ -121,7 +121,7 @@ const SentMSG = ()=>{
     
   const channel = '#sharkdefense-support'; 
   
-    const token ='xoxp-7169341236582-7188641415025-7214064832645-d44cd9a7da5276a21fb3a4ac15c3280e';
+    // const token ='xoxp-7169341236582-7188641415025-7214064832645-d44cd9a7da5276a21fb3a4ac15c3280e';
     
     
     const sendMessage = async () => {
